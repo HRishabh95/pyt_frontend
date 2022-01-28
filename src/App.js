a5151368ddc53c1c856b60f42148384ed12ed63c
uhrishabh <uhrishabh@gmail.com>
2022-01-28 14:51:01 +0100

    Added expandable div

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Component} from "react";
 import React from "react";
 import {Button} from "react-native";
 import {Collapse} from "react-collapse/lib/Collapse";
-
+import './App.css';
 
 
 export default class App extends Component {
@@ -13,16 +13,30 @@ export default class App extends Component {
         checked: false,
     };
 
+    constructor(props){
+            super(props);
+            this.state = {
+                    open: false
+                }
+                this.togglePanel = this.togglePanel.bind(this);
+        };
 
-  handleOnChange = event => {
-    this.setState({ searchValue: event.target.value });
-  };
+    togglePanel(e){
+        this.setState({open: !this.state.open})
+        };
+
+    handleOnChange = event => {
+        this.setState({ searchValue: event.target.value });
+    };
+
+
 
   render(){
+
       return (
         <div>
-          <h1>Welcome to the Pyterrier Test Search engine</h1>
-            <input
+          <h1 style={{text_align:"center"}}>Welcome to the Pyterrier Test Search engine</h1>
+            <input style={{width: "50%", margin:'auto'}}
               name="text"
               type="text"
               placeholder="Search"
@@ -30,23 +44,28 @@ export default class App extends Component {
               value={this.state.searchValue}
             />
 
-            <label>
-            <input type="checkbox"
+            <div className='advance_container'>
+             <button onClick={(e)=> this.togglePanel(e)} className='header'>
+                 Advance options
+             </button>
+                {this.state.open? (
+            <label className='checkbox-label'>
+            <input className='checkboxes' type="checkbox"
             defaultChecked={this.state.checked}
             onChange={() => this.setState({checked:!this.state.checked})}
             />
             Misinformation
-            </label>
+            </label>):null} </div>
           <nav
             style={{
               borderBottom: "solid 1px",
-              paddingBottom: "1rem"
+              paddingBottom: "1rem",
             }}
           >
             {/*<Link to="/invoices">Invoices</Link> |{" "}*/}
-            <Link style={{ display: "block", margin: "1rem 0" }}
+            <Link style={{ display: "block", margin_left: "50px 50px", text_align:"center" }}
                   to={`/search/${this.state.searchValue}`}
-                  key={this.state.searchValue}>Search</Link>
+                  key={this.state.searchValue}><button>Search</button></Link>
           </nav>
           <Outlet />
         </div>
--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,20 @@
-import {Outlet, Link, useParams} from "react-router-dom";
+import {Outlet, NavLink, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import React from 'react';
 import axios from 'axios';
+import useCollapse from 'react-collapsed';
+
+
+
+
 
 const Search = () => {
     let params = useParams();
     const [docs, setdocs] = useState([]);
+    let [sortType, setSortType] = useState('Topical');
     useEffect(() => {
         fetchdocs();
+        // sortArray(sortType);
     }, []);
     const fetchdocs = () => {
         axios
@@ -20,20 +27,35 @@ const Search = () => {
                 console.log(err);
             });
     };
+    //
+    // const sortArray = type =>{
+    //     const types= {
+    //         Topical:'score',
+    //         Credible:'cred_score',
+    //     };
+    //     const sortProperty = types[type];
+    //     const sorted =[...docs].sort((a,b) => b[sortProperty]-a[sortProperty]);
+    //     setdocs(sorted);
+    // };
     return (
-            <div style={{ display: "flex",overflow: "scroll"}}>
+            <div style={{display:'flex'}}>
        <nav
          style={{
            borderRight: "solid 1px",
-           padding: "1rem"
-         }}>
+           padding: "1rem"}}>
+           {/*<select onChange={(e) => setSortType(e.target.value)}>*/}
+           {/*    <option value='Topical'>Topical</option>*/}
+           {/*    <option value='Credible'>Credible</option>*/}
+           {/*</select>*/}
             <h1>Results</h1>
-                {docs.map((doc) => (
-                    <Link style={{ display: "block", margin: "1rem 0"}}
+                {docs
+                    .sort((a,b)=>(a[sortType] > b[sortType]) ? -1 : 1)
+                    .map((doc) => (
+                    <NavLink style={{ display: "block", margin: "1rem 0"}}
                           to={`/search/${params.searchValue}/${doc.docno}`}
                           state={{text: doc.text, score:doc.score, term:params.searchValue, misinfo_score:doc.cred_score}}>
                         <h3>{doc.docno}</h3>
-                    </Link>
+                    </NavLink>
                 ))}
            </nav>
                 <Outlet />
